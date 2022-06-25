3bcc90cad3a0a3b24bfffac59de8a20faf85c1a1
Giovanni Pignataro <giovanni.pignataro@MacBook-Pro-di-DecHit.local>
2022-06-25 02:55:04 +0200

    remove ssr in home

---
+++ components/organisms/Main/Main.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Main: NextPage = () => {
+  return <main>main</main>;
+};
+
+export default Main;
--- components/templates/Home/Home.tsx
+++ components/templates/Home/Home.tsx
@@ -1,23 +1,14 @@
 import type { NextPage } from "next";
 import Header from "../../organisms/Header/Header";
+import Main from "../../organisms/Main/Main";
 import Footer from "../../organisms/Footer/Footer";
 import { HomeProps } from "../../../types/types";
-
-export async function getServerSideProps() {
-  const defaultEndpoint = "http://localhost:4000/app";
-  const res = await fetch(defaultEndpoint);
-  const data = await res.json();
-
-  return { props: data };
-}
 
 const Home: NextPage<HomeProps> = (props) => {
   return (
     <>
       <Header {...props.header} />
-
-      {/* <main>main</main> */}
-
+      <Main />
       <Footer />
     </>
   );
